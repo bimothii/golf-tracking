59846876180b5f65ed584de4e0f054c2099306c0
bimothii <briank1727@gmail.com>
2024-08-30 10:53:56 -0500

    Initial Commit

---
+++ Models/Hole.swift
@@ -0,0 +1,8 @@
+//
+//  Hole.swift
+//  PracticumProject
+//
+//  Created by Brian Kim on 8/30/24.
+//
+
+import Foundation
---
+++ ViewModels/AddHoleViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddHoleViewModel.swift
+//  PracticumProject
+//
+//  Created by Brian Kim on 8/30/24.
+//
+
+import Foundation
